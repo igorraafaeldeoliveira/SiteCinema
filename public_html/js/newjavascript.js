b06071440b738c93c75208e5367ebe07a1e79eae
Igor Rafael De Oliveira <igor.oliveira@aluno.sc.senac.br>
2018-11-28 22:06:38 -0200

    versão final (sem o mobile)

--- public_html/js/newjavascript.js
+++ public_html/js/newjavascript.js
@@ -1,7 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function section(to) {
+    $("html, body") .animate({ scrollTop:to }, 600);   /* 600 mudar velocidade  */
+    return false;
+}
